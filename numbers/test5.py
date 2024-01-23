9869d2d3f45250badb267acfe893cc4184ba2b26
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-01-23 11:46:47 +0800

    String Methods

---
+++ numbers/test5.py
@@ -0,0 +1,5 @@
+text = "   Python is great!   "
+stripped_text = text.strip()  # Removes leading and trailing spaces
+upper_case = text.upper()  # Converts to uppercase
+lower_case = text.lower()  # Converts to lowercase
+replaced_text = text.replace("great", "awesome")  # Replaces text
