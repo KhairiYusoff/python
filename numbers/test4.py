8bc09b9a1c315466990b71d5e25d1ab3dd412099
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-01-23 11:46:08 +0800

    String Indexing and Slicing

---
+++ numbers/test4.py
@@ -0,0 +1,6 @@
+
+text = "Hello, Python"
+first_char = text[0]  # Accessing the first character
+sub_string = text[7:12]  # Slicing a substring
+print(first_char)  # Output: "H"
+print(sub_string)  # Output: "Pytho"
